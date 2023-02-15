e1da131fd99f7c6d63e0441a4488ffc2f511d499
udras eva <doors-13@inbox.ru>
2023-02-15 17:03:32 +0300

    update

--- Simplicial_complex.hpp
+++ Simplicial_complex.hpp
@@ -1,13 +1,107 @@
 #ifndef SIMPLICIAL_COMPLEX_HPP
 #define SIMPLICIAL_COMPLEX_HPP
 
+#include <boost/hana/assert.hpp>
+#include <boost/hana/for_each.hpp>
+#include <boost/hana/core/make.hpp>
+#include <boost/hana/equal.hpp>
+#include <boost/hana/tuple.hpp>
+#include <boost/hana/integral_constant.hpp>
+#include <boost/hana/map.hpp>
+#include <boost/hana/pair.hpp>
+#include <boost/hana/type.hpp>
+#include <boost/hana/fold_left.hpp>
+#include <boost/hana/insert.hpp>
+#include <boost/hana/remove_if.hpp>
+#include <boost/hana/functional/compose.hpp>
+#include <boost/hana/functional/iterate.hpp>
+#include <boost/hana/functional/placeholder.hpp>
+#include <boost/hana/fuse.hpp>
+#include <boost/hana/at.hpp>
+#include <boost/hana/traits.hpp>
+#include <boost/core/typeinfo.hpp>
+#include <variant>
+
 #include "Simplex.hpp"
 
-template <int N, typename T>
-class Simplicial_complex
+
+namespace hana = boost::hana;
+namespace mpl  = boost::mpl;
+
+struct print_type
 {
+    template <class T>
+    void operator() (T) const
+    {
+        auto const& ti = BOOST_CORE_TYPEID(T);
+        std::cout << boost::core::demangled_name(ti) << std::endl;
+    }
+};
+
+template<typename ...Args>
+inline auto make_complex(Args&&... args) {
+    constexpr auto complex_type = hana::make<hana::tuple_tag>(hana::type<Args>{}...);
+    constexpr auto complex_dim  = hana::make<hana::tuple_tag>(hana::int_c<std::decay_t<Args>::dim>...);
+    auto complex = hana::make<hana::tuple_tag>(std::forward<Args>(args)...);
+    hana::for_each(complex_type, print_type());
+    hana::for_each(complex_dim, [&](auto x) {
+        std::cout << "Simplex dim = " << x << '\n';
+    });
+    std::cout << std::endl;
+//    hana::for_each(complex, [&](auto x) {
+//        if constexpr(decltype(x)::dim == 2)
+//        std::cout << x << ' ';
+//    });
+    std::cout << std::endl;
+    return hana::make_tuple(complex_type, complex_dim, complex);
+}
+template <class _Ty, _Ty _Val>
+struct N_constant {
+    static constexpr _Ty value = _Val;
+
+    using value_type = _Ty;
+    using type       = N_constant;
+
+    constexpr operator value_type() const noexcept {
+        return value;
+    }
+
+    _NODISCARD constexpr value_type operator()() const noexcept {
+        return value;
+    }
+};
+
+template <bool _Val>
+using bool_constant = N_constant<bool, _Val>;
+
+using true_type  = bool_constant<true>;
+using false_type = bool_constant<false>;
+
+template <class _Ty, int N>
+inline constexpr bool is_N_v = _Ty::dim == N;
+
+template <class Complex>
+class Simplicial_complex {
+private:
+     Complex& complex;
+     template<int N>
+     struct is_NN {
+         template <class _Ty>
+         struct is_N : bool_constant<is_N_v<_Ty, N>> {};
+     };
+
 public:
-    Simplicial_complex() {}
+    Simplicial_complex(Complex& complex_) : complex(complex_) {}
+    template<int N>
+    auto get_complex() const {
+
+        constexpr auto is_N__ = hana::compose(hana::trait<is_NN<N>::template is_N>, hana::decltype_);
+        auto ret = hana::filter(hana::at(this->complex, hana::size_c<2>), is_N__);
+        hana::for_each(ret, [&](auto x) {
+            std::cout << "Simplex dim = " << decltype(x)::dim << "; value = " << x << '\n';
+        });
+        std::cout << std::endl;
+    }
 };
 
 #endif // SIMPLICIAL_COMPLEX_HPP
--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 
 #include "Simplicial_complex.hpp"
 #include "Vector_space.hpp"
-
+static constexpr int N = 4;
 int main()
 {
+    Vector_space<4, int> d0(0, 0, 0, 0);
+    Vector_space<4, int> d1(1, 0, 0, 0);
+    Vector_space<4, int> d2(0, 1, 0, 0);
+    Vector_space<4, int> d3(0, 0, 1, 0);
+    Vector_space<4, int> d4(0, 0, 0, 1);
+
     Vector_space<3, int> a0(0, 0, 0);
     Vector_space<3, int> a1(1, 0, 0);
     Vector_space<3, int> a2(0, 1, 0);
@@ -13,17 +19,35 @@ int main()
     Vector_space<2, int> b0(0, 0);
     Vector_space<2, int> b1(1, 0);
     Vector_space<2, int> b2(0, 1);
-//    std::cout << a0 << std::endl;
-//    Simplex<6, int> simplex(1, 2, 3, 4, 5, 6, 7);
-    Simplex<3, Vector_space<3, int>> simplex3D(a0*2, a1*(-2), a2*8, a3*(-3));
-    Simplex<3, Vector_space<3, int>> simplex3D1(a0*4, a2*(-6), a1*8, a3*(-3));
-    Simplex<2, Vector_space<2, int>> simplex2D(b0*2, b1*(-2), b2*8);
-    auto tmp = (simplex3D - simplex3D);
-    auto tmp1 = (simplex3D - simplex3D1);
-    std::cout << "CHAIN: " << tmp1 << std::endl;
+
+    Vector_space<1, int> c0(1);
+    Vector_space<1, int> c1(0);
+
+    Simplex<4, Vector_space<4, int>> simplex4D_0(d0*2, d1*(-2), d2*8, d3*(-3), d4*(-2));
+    Simplex<4, Vector_space<4, int>> simplex4D_1(d0*4, d2*(-6), d1*8, d3*(-3), d4*8);
+
+    Simplex<3, Vector_space<3, int>> simplex3D_0(a0*2, a1*(-2), a2*8, a3*(-3));
+    Simplex<3, Vector_space<3, int>> simplex3D_1(a0*4, a2*(-6), a1*8, a3*(-3));
+    Simplex<2, Vector_space<2, int>> simplex2D_0(b0*2, b1*(-2), b2*8);
+    Simplex<2, Vector_space<2, int>> simplex2D_1(b0*2, b1*(2), b2*5);
+    Simplex<1, Vector_space<1, int>> simplex1D_0(c0*2, c1*(-2));
+
+    Simplex<0, Vector_space<1, int>> simplex0D_0(c0*2);
+    Simplex<0, Vector_space<1, int>> simplex0D_1(c0*2);
+    Simplex<0, Vector_space<1, int>> simplex0D_2(c0*2);
+
+    auto tmp = (simplex3D_0 - simplex3D_1);
+    auto tmp1 = (simplex3D_1 - simplex3D_0);
+    std::cout << "START MAKE COMPLEX" << std::endl;
+    auto complex = make_complex(simplex4D_0, simplex4D_1, simplex3D_0, simplex3D_1, simplex2D_0, simplex2D_1, simplex1D_0, simplex0D_1, simplex0D_2);
+    std::cout << "END MAKE COMPLEX" << std::endl;
+    std::cout << "GET COMPLEX DIMENTION = " << N << std::endl;
+    Simplicial_complex complex_simpl(complex);
+    complex_simpl.get_complex<N>();
+    std::cout << "END COMPLEX DIMENTION = " << N << std::endl;
 //    simplex3D.boundary();
 //    simplex2D.boundary();
-    auto tmp3 = simplex3D.boundary()*simplex2D.boundary();
-    std::cout << "dif(N)*dif(N - 1) = " << tmp3 << std::endl;
+//    auto tmp3 = simplex3D.boundary()*simplex2D.boundary();
+//    std::cout << "dif(N)*dif(N - 1) = " << tmp3 << std::endl;
     return 0;
 }
